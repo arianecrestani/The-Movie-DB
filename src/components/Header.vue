88797d280ac4de89fb1ddf4190a07d4f28d67f5e
arianecrestani <ariane.mrv@gmail.com>
2021-08-19 18:20:48 +0200

    add style Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,7 @@
   <header>
     <nav>
    
+   <img alt="logo-Imdb" id="logo" src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg"><img>
    
     </nav>
 
@@ -23,5 +24,8 @@ export default {
         height: 60px;
         width: 100%;
     }
-
+    #logo {
+        
+        width:5em;
+    }
 </style>
