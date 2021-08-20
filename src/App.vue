422bbc9c94c5590edf180d21d319f1d3926ee6da
arianecrestani <ariane.mrv@gmail.com>
2021-08-20 18:38:37 +0200

    intall axios

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
     <Header />
+  <h1>Movies</h1>
+  <button v-on:click="loadMovies">laden...</button>
   </div>
 </template>
 
@@ -11,6 +13,11 @@ import'./src/style/global.css';
 import Header from './components/Header';
 export default {
   name: 'App',
+  methods: {
+    loadMovies(){
+      console.log("button wurde geglickt")
+    }
+  },
   components: {
     Header
   }
