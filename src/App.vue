94adde20a1fe122034f71163fecad6ab0da5f489
arianecrestani <ariane.mrv@gmail.com>
2021-08-23 14:05:21 +0200

    refactored Api into methods

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <!-- <Header /> -->
-    <h1>Movies</h1>
-    <button v-on:click="loadMovies">laden...</button>
+  <h1>Movies</h1>
+  <button v-on:click="loadMovies">laden...</button>
   </div>
 </template>
 
@@ -10,31 +10,41 @@
 
 import'./src/style/global.css';
 
-const movie_Api = "af4c11d1c7c756c2429ca0c3cf65c08c";
-const baseUrl = (endpoint) => {
-  const baseUrl = "https://api.themoviedb.org/3";
-  const apiKey = `${movie_Api}`;
-  return `${baseUrl}/${endpoint}&api_key=${apiKey}`;
-};
+// const movie_Api = "af4c11d1c7c756c2429ca0c3cf65c08c";
+// const baseUrl = (endpoint) => {
+//   const baseUrl = "https://api.themoviedb.org/3";
+//   const apiKey = `${movie_Api}`;
+//   return `${baseUrl}/${endpoint}&api_key=${apiKey}`;
+// };
 
-const apiRequest = async (endpoint) => {
-  return await this.axios.get(baseUrl(endpoint))
-    .then((response) => response.json())
-    .catch((error) => console.log(error));
-};
-
-const getMovie = (query) => `search/movie?language=en-US&query=${query}&page=1`;
+// const apiRequest = async (endpoint) => {
+//   return await fetch(baseUrl(endpoint))
+//     .then((response) => response.json())
+//     .catch((error) => console.log(error));
+// };
 
 // import Header from './components/Header';
 export default {
   name: 'App',
   methods: {
-    async loadMovies() {
-        apiRequest(getMovie("lord of the rings")) // trocar por text field
+    
+   async loadMovies(){
+    let apiUrl =  "https://api.themoviedb.org/3/search/movie?language=en-US&query=${query}&page=1&api_key=af4c11d1c7c756c2429ca0c3cf65c08c"
+    console.log("button wurde geglickt");
+    this.axios.get(apiUrl).then((response) => {
+        console.log(response.data)
+      });
+    return await this.axios.get(apiUrl)
+    .then((response) => response.json())
+    .catch((error) => console.log(error));
+
+      }
     }
   }
-}
   // components: {
   //   Header
   // }
+
 </script>
+
+
