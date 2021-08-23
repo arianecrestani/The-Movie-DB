af55524a731c6bf658441524fd92fa0e77c5c699
arianecrestani <ariane.mrv@gmail.com>
2021-08-23 13:36:48 +0200

    add api movie db

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <Header />
-  <h1>Movies</h1>
-  <button v-on:click="loadMovies">laden...</button>
+    <!-- <Header /> -->
+    <h1>Movies</h1>
+    <button v-on:click="loadMovies">laden...</button>
   </div>
 </template>
 
@@ -10,18 +10,31 @@
 
 import'./src/style/global.css';
 
-import Header from './components/Header';
+const movie_Api = "af4c11d1c7c756c2429ca0c3cf65c08c";
+const baseUrl = (endpoint) => {
+  const baseUrl = "https://api.themoviedb.org/3";
+  const apiKey = `${movie_Api}`;
+  return `${baseUrl}/${endpoint}&api_key=${apiKey}`;
+};
+
+const apiRequest = async (endpoint) => {
+  return await this.axios.get(baseUrl(endpoint))
+    .then((response) => response.json())
+    .catch((error) => console.log(error));
+};
+
+const getMovie = (query) => `search/movie?language=en-US&query=${query}&page=1`;
+
+// import Header from './components/Header';
 export default {
   name: 'App',
   methods: {
-    loadMovies(){
-      console.log("button wurde geglickt")
+    async loadMovies() {
+        apiRequest(getMovie("lord of the rings")) // trocar por text field
     }
-  },
-  components: {
-    Header
   }
 }
+  // components: {
+  //   Header
+  // }
 </script>
-
-
