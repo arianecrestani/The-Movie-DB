7e2907998e3081ba178256630fd2f20b1bd60c1a
arianecrestani <ariane.mrv@gmail.com>
2021-08-24 22:30:44 +0200

    set api

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,44 @@
 <template>
   <div id="app">
     <!-- <Header /> -->
-  <h1>Movies</h1>
-  <button v-on:click="loadMovies">laden...</button>
+    <h1>Movies</h1>
+    <button v-on:click="loadMovies">laden...</button>
+    <ul>
+      <li v-for="result in results" v-bind:key="result.id">
+        {{result.title}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-
-import'./src/style/global.css';
-
-// const movie_Api = "af4c11d1c7c756c2429ca0c3cf65c08c";
-// const baseUrl = (endpoint) => {
-//   const baseUrl = "https://api.themoviedb.org/3";
-//   const apiKey = `${movie_Api}`;
-//   return `${baseUrl}/${endpoint}&api_key=${apiKey}`;
-// };
-
-// const apiRequest = async (endpoint) => {
-//   return await fetch(baseUrl(endpoint))
-//     .then((response) => response.json())
-//     .catch((error) => console.log(error));
-// };
+import "./src/style/global.css";
 
 // import Header from './components/Header';
 export default {
-  name: 'App',
+  name: "App",
+  data: () => {
+    return {
+      results: [],
+    };
+  },
   methods: {
-    
-   async loadMovies(){
-    let apiUrl =  "https://api.themoviedb.org/3/search/movie?language=en-US&query=${query}&page=1&api_key=af4c11d1c7c756c2429ca0c3cf65c08c"
-    console.log("button wurde geglickt");
-    this.axios.get(apiUrl).then((response) => {
-        console.log(response.data)
-      });
-    return await this.axios.get(apiUrl)
-    .then((response) => response.json())
-    .catch((error) => console.log(error));
-
+    async loadMovies() {
+      const query = "lord of the rings";
+      let apiUrl =
+        `https://api.themoviedb.org/3/search/movie?language=en-US&query=${query}&page=1&api_key=af4c11d1c7c756c2429ca0c3cf65c08c`;
+      try {
+        let response = await this.axios.get(apiUrl);
+        console.log(response.data.results);
+        this.results = response.data.results;
+      } catch (e) {
+        console.error(e);
       }
-    }
-  }
-  // components: {
-  //   Header
-  // }
+    },
+  },
+};
 
+// components: {
+//   Header
+// }
 </script>
-
-
