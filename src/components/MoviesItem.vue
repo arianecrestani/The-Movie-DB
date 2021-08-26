46244ae0a8950f80ee3d44556a67fd73c658bfce
arianecrestani <ariane.mrv@gmail.com>
2021-08-26 19:04:09 +0200

    add style test css

--- src/components/MoviesItem.vue
+++ src/components/MoviesItem.vue
@@ -1,18 +1,18 @@
 <template>
-  <li :class= "{'title' :movie.title}">
-      {{movie.original_title}}.{{movie.original_title}}
-  </li>
-
-  
+  <div :class="{ title: movie.title }">
+    {{ movie.original_title }}
+  </div>
 </template>
 
 <script>
-
 export default {
-    props: {
-        movie:Object
-    }
-    
-   
+  props: {
+    movie: Object,
+  },
 };
 </script>
+<style scoped>
+.title {
+  color: brown;
+}
+</style>
