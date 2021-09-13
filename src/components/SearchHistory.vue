a3955012e45d3bdc487269b5db80192bd8c04a07
arianecrestani <ariane.mrv@gmail.com>
2021-09-13 14:29:08 +0200

    remove items of search history(splice)

--- src/components/SearchHistory.vue
+++ src/components/SearchHistory.vue
@@ -1,26 +1,29 @@
 <template>
   <div class=" bg-gray-100 w-2/4  list-none cursor-default">
-    <ul v-for="(item, index) in searchHistory" v-bind:key="index" >
-      <li class="hover:bg-green-700 p-3" @click="setResult(item)" >{{ item }}</li>
+    <ul v-for="(item, index) in searchHistory" v-bind:key="index">
+      <li class="hover:bg-green-700 p-3" @click="setResult(item)">
+        {{ item }}
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
-
 export default {
   name: "SearchHistory",
   methods: {
     setResult(item) {
-      this.$emit('valueselected', item)
-    }
+      
+     this.$emit("valueselected", item);
+      this.searchHistory.splice(item);
+
+    },
   },
   computed: {
     searchHistory: function() {
-      return this.$store.state.searchHistory
-      
-    }
-  }
+      return this.$store.state.searchHistory;
+    },
+  },
 };
 </script>
 
