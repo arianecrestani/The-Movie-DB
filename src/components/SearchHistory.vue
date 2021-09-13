62487d695b3ae20bf14e22bc7d0e6abf2d2fac7b
arianecrestani <ariane.mrv@gmail.com>
2021-09-13 21:31:40 +0200

    created method slice and reverse to search history

--- src/components/SearchHistory.vue
+++ src/components/SearchHistory.vue
@@ -1,6 +1,6 @@
 <template>
   <div class=" bg-gray-100 w-2/4  list-none cursor-default">
-    <ul v-for="(item, index) in searchHistory" v-bind:key="index">
+    <ul  v-for="(item, index) in searchHistory" v-bind:key="index">
       <li class="hover:bg-green-700 p-3" @click="setResult(item)">
         {{ item }}
       </li>
@@ -13,15 +13,14 @@ export default {
   name: "SearchHistory",
   methods: {
     setResult(item) {
-      
-     this.$emit("valueselected", item);
-      this.searchHistory.splice(item);
-
+      this.$emit("valueselected", item);
     },
   },
   computed: {
     searchHistory: function() {
-      return this.$store.state.searchHistory;
+      const searchHistory = this.$store.state.searchHistory
+      return searchHistory.reverse().slice(0,4)
+      
     },
   },
 };
