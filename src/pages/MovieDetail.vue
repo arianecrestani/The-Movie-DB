30048b3dd28744163e51a0d93f210b40c7bc9e04
Ariane Crestani <ariane.mrv@gmail.com>
2021-10-06 19:43:59 +0200

    some changes

--- src/pages/MovieDetail.vue
+++ src/pages/MovieDetail.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
     <Header />
-    <router-link to= "/" active-class="active">
-      <a class="bg-green-300">home</a></router-link>
-
+    <div class="flex flex-col justify-center py-12 items-center">
+      <router-link to="/" active-class="active">
+        <a class="bg-green-300">home</a></router-link
+      >
+      <button back @click="backToHome">backToHome</button>
       <h3>{{ movie.title }}</h3>
-      <h3>{{ movie.overview}}</h3>
-    <!-- show Image -->
-    <!-- show title -->
-    <!-- show description -->
-    <!-- show rate -->
+      <div class="movie flex-col bg-gray-100 py-9 rounded-md  text-center">
+        <h3>{{ movie.overview }}</h3>
+      </div>
+      <!-- show Image -->
+      <!-- show title -->
+      <!-- show description -->
+      <!-- show rate -->
+    </div>
   </div>
 </template>
 
@@ -18,20 +23,23 @@ import Header from "../components/Header.vue";
 export default {
   name: "MovieDetail",
   props: {
-    movie: Object
+    movie: Object,
   },
   components: {
     Header,
   },
   methods: {
-  // async loadMovie() {
-  //    const movies = this.$store.state.movies
-  //     // storage
-  //     // search movie
-  //       // for, serch find,  
-  //     // movie.title
-  //     // this.movie = filqueque tu achou no array
-  //   },
+    // async loadMovie() {
+    //    const movies = this.$store.state.movies
+    //     // storage
+    //     // search movie
+    //       // for, serch find,
+    //     // movie.title
+    //     // this.movie = filqueque tu achou no array
+    //   },
+    backToHome() {
+      this.$router.push({ path: "/" });
+    },
   },
   data() {
     return {
